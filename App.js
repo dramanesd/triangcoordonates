550f839e74fe459896526cb190bec4f2d1a434aa
Dramane DOUMBIA <ddramane63@gmail.com>
2019-11-07 23:04:59 +0000

    initial commit

---
+++ App.js
@@ -0,0 +1,31 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.body}>
+        <Text>Hello Dramane</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  body: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+});
+
+export default App;
