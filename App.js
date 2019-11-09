dc2b7e897561cff8a603ad78e097900fe0199811
Dramane DOUMBIA <ddramane63@gmail.com>
2019-11-09 01:11:08 +0000

    Imgration Success

--- App.js
+++ App.js
@@ -1,31 +1,106 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, {Component} from 'react';
+import {createAppContainer} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+import {createStackNavigator} from 'react-navigation-stack';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import SplashScreen from 'react-native-splash-screen';
 
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import Home from './App/Components/Home';
+import AddForm from './App/Components/AddForm';
+import About from './App/Components/About';
 
-class App extends React.Component {
+const HomeStack = createStackNavigator(
+  {
+    //Defination of Navigaton from home screen
+    Home: {screen: Home},
+    Add: {screen: AddForm},
+    Me: {screen: About},
+  },
+  {
+    defaultNavigationOptions: ({navigation}) => ({
+      // //Header customization of the perticular Screen
+      headerStyle: {
+        backgroundColor: '#7B1FA2',
+      },
+      headerTintColor: '#FFFFFF',
+      title: 'Coordonnées',
+      headerRight: () => (
+        <Icon.Button
+          type={'MaterialIcons'}
+          name={'add-circle'}
+          size={30}
+          underlayColor={'transparent'}
+          backgroundColor={'transparent'}
+          onPress={() => navigation.navigate('Add')}
+        />
+      ),
+    }),
+  },
+);
+
+HomeStack.navigationOptions = ({navigation}) => {
+  let tabBarVisible = true;
+  if (navigation.state.index > 0) {
+    tabBarVisible = false;
+  }
+
+  return {
+    tabBarVisible,
+  };
+};
+
+const TabNavigator = createBottomTabNavigator(
+  {
+    Home: {screen: HomeStack},
+    Add: {screen: AddForm},
+    Me: {screen: About},
+  },
+  {
+    defaultNavigationOptions: ({navigation}) => ({
+      tabBarIcon: ({focused, horizontal, tintColor}) => {
+        const {routeName} = navigation.state;
+        let IconComponent = Icon;
+        let iconName;
+        if (routeName === 'Home') {
+          iconName = `home${focused ? '' : ''}`;
+        } else if (routeName === 'Add') {
+          iconName = `add-circle${focused ? '' : '-outline'}`;
+        } else if (routeName === 'Me') {
+          iconName = `info${focused ? '' : '-outline'}`;
+        }
+        return (
+          <IconComponent
+            type={'MaterialIcons'}
+            name={iconName}
+            size={25}
+            color={tintColor}
+          />
+        );
+      },
+    }),
+    tabBarOptions: {
+      activeTintColor: '#FFF',
+      inactiveTintColor: 'gray',
+      style: {
+        backgroundColor: '#7B1FA2',
+        borderColor: 'transparent',
+      },
+      showLabel: false,
+    },
+  },
+);
+
+const AppContainer = createAppContainer(TabNavigator);
+
+class App extends Component {
+
+  componentDidMount() {
+    SplashScreen.hide();
+  }
+
   render() {
-    return (
-      <View style={styles.body}>
-        <Text>Hello Dramane!</Text>
-      </View>
-    );
+    return <AppContainer />;
   }
 }
-
-const styles = StyleSheet.create({
-  body: {
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-});
 
 export default App;
